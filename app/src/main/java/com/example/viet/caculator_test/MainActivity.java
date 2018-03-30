62135fef5aa33a43575657045a2167c7588a523a
vietcrit96 <37839562+vietcrit96@users.noreply.github.com>
2018-03-31 00:59:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/viet/caculator_test/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.viet.caculator_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtNumber;
+    private TextView tvResult;
+    private Button btnClear;
+    private Button btnRemove;
+    private Button btnPer;
+    private Button btnDot;
+    private Button btnEqual;
+
+    private Button btnDivi;
+    private Button btnMulti;
+    private Button btnMinus;
+    private Button btnPlus;
+
+    private Button btnNumber7;
+    private Button btnNumber8;
+    private Button btnNumber9;
+    private Button btnNumber4;
+    private Button btnNumber5;
+    private Button btnNumber6;
+    private Button btnNumber1;
+    private Button btnNumber2;
+    private Button btnNumber3;
+    private Button btnYinyang;
+    private Button btnNumber0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
